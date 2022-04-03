2d4b2a7661617af97bd73416ae1e56b59090e4d2
Abeer Naeem <abeernaeem6@gmail.com>
2022-04-03 00:55:49 -0500

    Added file generator and readme template

---
+++ Develop/TEST1.md
@@ -0,0 +1,38 @@
+
+# TEST1
+
+## Description
+
+TEST1
+    
+## Table of Contents
+    
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+    
+## Installation
+
+TEST1
+    
+## Usage
+    
+TEST1
+
+## Credits
+
+TEST1
+    
+## License
+
+npm-packages
+    
+## Badges
+    
+![badmath](https://shields.io/badge/npm-packages-blue)
+    
+## Tests
+
+TEST1
+    
--- Develop/index.js
+++ Develop/index.js
@@ -1,124 +1,135 @@
 // TODO: Include packages needed for this application
 
 const inquirer = require("inquirer");
-const fs = require('fs');
-
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        message: "What is then name of your Project?",
-        name: 'projectTitle',
-    },
+  {
+    type: "input",
+    message: "What is then name of your Project?",
+    name: "projectTitle",
+  },
 
-    {
-        type: 'input',
-        message: 'Please describe your project',
-        name: 'description',
-    },
+  {
+    type: "input",
+    message: "Please describe your project",
+    name: "description",
+  },
 
-    {
-        type: 'input',
-        message: 'Please input the installation instructions',
-        name: 'installation',
-    },
+  {
+    type: "input",
+    message: "Please input the installation instructions",
+    name: "installation",
+  },
 
-    {
-        type: 'input',
-        message: 'Please input the usage information',
-        name: 'usage',
-    },
+  {
+    type: "input",
+    message: "Please input the usage information",
+    name: "usage",
+  },
 
-    {
-        type: 'input',
-        message: 'Please input the contribution guidelines',
-        name: 'contributionGuidelines',
-    },
+  {
+    type: "input",
+    message: "Please input the contribution guidelines",
+    name: "contributionGuidelines",
+  },
 
-    {
-        type: 'input',
-        message: 'Please input the test instructions',
-        name: 'test',
-    },
+  {
+    type: "input",
+    message: "Please input the test instructions",
+    name: "test",
+  },
 
-    {
-        type: 'list',
-        message: 'Please choose the license for your project',
-        name: 'licence',
-        choices: ['Mit License','GNU GPLv3', 'Apache', 'Cloud Native Computing Foundation', 'GNU', 'npm packages', 'OpenBSD', 'Rust', 'WordPress' ]
-    },
+  {
+    type: "list",
+    message: "Please choose the license for your project",
+    name: "licence",
+    choices: [
+      "Mit-License",
+      "GNU-GPLv3",
+      "Apache",
+      "Cloud_Native_Computing_Foundation",
+      "GNU",
+      "npm-packages",
+      "OpenBSD",
+      "Rust",
+      "WordPress",
+    ],
+  },
 
-    {
-        type: 'input',
-        message: 'Please enter your github username',
-        name: 'userName'
-    },
+  {
+    type: "input",
+    message: "Please enter your github username",
+    name: "userName",
+  },
 
-    {
-        type: 'input',
-        message: 'Please enter your email address',
-        name: 'email'
+  {
+    type: "input",
+    message: "Please enter your email address",
+    name: "email",
+  },
 
-    }
-
+  {
+    type: "input",
+    message: "Please enter the name for your README file",
+    name: "name",
+  },
 ];
 
-inquirer
-    .prompt(questions)
-    .then ((response) => {
-        console.log(response)
-        writeToFile('README.md',response)
-    })
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
-    const template = `
+  const template = `
 # ${data.projectTitle}
 
 ## Description
 
-    ${data.description}
+${data.description}
     
 ## Table of Contents
     
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Credits](#credits)
-    - [License](#license)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
     
 ## Installation
 
-    ${data.installation}
+${data.installation}
     
 ## Usage
     
-    ${data.usage}
+${data.usage}
 
 ## Credits
 
-    ${data.contributionGuidelines}
+${data.contributionGuidelines}
     
 ## License
 
-    ${data.licence}
+${data.licence}
     
 ## Badges
     
-    ![badmath](https://shields.io/badge/${data.licence}-blue)
+![badmath](https://shields.io/badge/${data.licence}-blue)
     
 ## Tests
 
-    ${data.test}
-    `
-    fs.appendFile(fileName, template, (err) =>
-    err ? console.error(err) : console.log('Commit logged!')
-    )
+${data.test}
+    `;
+  fs.writeFile(fileName, template, (err) =>
+    err ? console.error(err) : console.log("Commit logged!")
+  );
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        console.log(response);
+        writeToFile(`${response.name}.md`, response);
+      });
+}
 
 // Function call to initialize app
 init();
